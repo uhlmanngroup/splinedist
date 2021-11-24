8eeff9e9541977ab327d49cd6d6f55da76fa7962
Soham Mandal <smandal1215@gmail.com>
2021-11-24 11:42:44 +0000

    Fix import

--- splinedist/big.py
+++ splinedist/big.py
@@ -7,7 +7,7 @@ from skimage.draw import polygon
 from csbdeep.utils import _raise, axes_check_and_normalize, axes_dict
 from itertools import product
 
-from .geometry import polygons_to_label, polyhedron_to_label
+from .geometry import polygons_to_label
 
 
 
@@ -318,7 +318,7 @@ class BlockND:
     def filter_objects(self, labels, polys, axes=None):
         """Filter out objects that block is not responsible for.
 
-        Given label image 'labels' and dictionary 'polys' of polygon/polyhedron objects,
+        Given label image 'labels' and dictionary 'polys' of polygon objects,
         only retain those objects that this block is responsible for.
 
         This function will return a pair (labels, polys) of the modified label image and dictionary.
@@ -449,33 +449,6 @@ class Polygon:
         mins = np.maximum(0,         np.floor(np.min(coord,axis=1))).astype(int)
         maxs = np.minimum(shape_max, np.ceil (np.max(coord,axis=1))).astype(int)
         return tuple(zip(tuple(mins),tuple(maxs)))
-
-
-
-class Polyhedron:
-
-    def __init__(self, dist, origin, rays, bbox=None, shape_max=None):
-        self.bbox = self.coords_bbox((dist, origin), rays=rays, shape_max=shape_max) if bbox is None else bbox
-        self.slice = tuple(slice(*r) for r in self.bbox)
-        self.shape = tuple(r[1]-r[0] for r in self.bbox)
-        _origin = origin.reshape(1,3) - np.array([r[0] for r in self.bbox]).reshape(1,3)
-        self.mask = polyhedron_to_label(dist[np.newaxis], _origin, rays, shape=self.shape, verbose=False).astype(np.bool)
-
-    @staticmethod
-    def coords_bbox(*dist_origin, rays, shape_max=None):
-        dists, points = zip(*dist_origin)
-        assert all(isinstance(d, np.ndarray) and d.ndim==1 and len(d)==len(rays) for d in dists)
-        assert all(isinstance(p, np.ndarray) and p.ndim==1 and len(p)==3 for p in points)
-        dists, points, verts = np.stack(dists)[...,np.newaxis], np.stack(points)[:,np.newaxis], rays.vertices[np.newaxis]
-        coord = dists * verts + points
-        coord = np.concatenate(coord, axis=0)
-        if shape_max is None:
-            shape_max = (np.inf, np.inf, np.inf)
-        mins = np.maximum(0,         np.floor(np.min(coord,axis=0))).astype(int)
-        maxs = np.minimum(shape_max, np.ceil (np.max(coord,axis=0))).astype(int)
-        return tuple(zip(tuple(mins),tuple(maxs)))
-
-
 
 # def repaint_labels(output, labels, polys, show_progress=True):
 #     """Repaint object instances in correct order based on probability scores.
